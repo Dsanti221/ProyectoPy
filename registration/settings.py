9ebb98641bc9b5c6f8cc88fe3055d4ede4dc6c99
Dsanti221 <david2vallejo@gmail.com>
2023-05-10 21:31:55 -0500

    Se agrega el proyecto

--- registration/settings.py
+++ registration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f&25drjcakda$z+5$21t$atgey4^32li0yv)vlstpk$m*e2yx4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://appcentrosvrm.azurewebsites.net']
 
 
 # Application definition
